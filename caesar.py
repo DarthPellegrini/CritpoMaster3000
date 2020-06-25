6b64d67ca7f7735801c33253b0d3dc9f4e62efe9
Leonardo Pellegrini Silva <pellegrinidev@outlook.com>
2020-06-25 18:18:32 -0300

    versao 1.7: fixed caesar cipher

--- caesar.py
+++ caesar.py
@@ -1,13 +1,14 @@
 def encrypt(plainText, shift, mode):
-    cipherText = '' 
-    for ch in plainText:
-        if ch.isalpha():
-            stayInAlphabet = ord(ch) + int(shift) 
-            if stayInAlphabet > ord('z'):
-                stayInAlphabet -= 26
-            finalLetter = chr(stayInAlphabet)
-        cipherText += finalLetter
-    return cipherText
+    cipher = ''
+    for char in plainText: 
+        if char == ' ':
+            cipher = cipher + char
+        elif  char.isupper():
+            cipher = cipher + chr((ord(char) + int(shift) - 65) % 26 + 65)
+        else:
+            print(shift)
+            cipher = cipher + chr((ord(char) + int(shift) - 97) % 26 + 97)
+    return cipher
 
 def decrypt(text, shift, mode):
     return encrypt(text,int(shift)*(-1), mode)
